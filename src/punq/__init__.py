a93e150cb3d0340014f5367c070038132765b92e
Bob Gregory <bob@made.com>
2017-09-07 09:05:42 +0100

    Moved signature inspection to registration time

--- src/punq/__init__.py
+++ src/punq/__init__.py
@@ -9,7 +9,7 @@ class InvalidRegistrationException (Exception):
     pass
 
 
-Registration = namedtuple('Registration', ['service', 'builder', 'args'])
+Registration = namedtuple('Registration', ['service', 'builder', 'needs', 'args'])
 
 
 class Registry:
@@ -17,18 +17,35 @@ class Registry:
     def __init__(self):
         self.__registrations = defaultdict(list)
 
+    def _get_needs_for_ctor(self, cls):
+        sig = typing.get_type_hints(cls.__init__)
+        return sig
+
     def register_service_and_impl(self, service, impl, resolve_args):
-        self.__registrations[service].append((impl, resolve_args))
+        self.__registrations[service].append(Registration(
+            service,
+            impl,
+            self._get_needs_for_ctor(impl),
+            resolve_args))
 
     def register_service_and_instance(self, service, instance):
-        self.__registrations[service].append(((lambda: instance), {}))
+        self.__registrations[service].append(Registration(
+            service,
+            lambda: instance,
+            {},
+            {}))
 
     def register_concrete_service(self, service):
         if not callable(service):
             raise InvalidRegistrationException(
                     "The service %s can't be registered as its own implementation" %
                     (repr(service)))
-        self.__registrations[service].append((service, {}))
+        self.__registrations[service].append(Registration(
+            service,
+            service,
+            self._get_needs_for_ctor(service),
+            {}))
+
 
     def register(self, service, factory=None, resolve_args=None):
         resolve_args = resolve_args or {}
@@ -42,6 +59,10 @@ class Registry:
     def __getitem__(self, service):
         return self.__registrations[service]
 
+    @property
+    def registrations(self):
+        return typing.MappingProxyType(self.__registrations)
+
 
 class Container:
 
@@ -53,25 +74,24 @@ class Container:
 
     def resolve_all(self, service):
         return [
-            self.build_impl(x, args) for x, args in self.registrations[service]
+            self.build_impl(x) for x in self.registrations[service]
         ]
 
-    def build_impl(self, factory, resolve_args):
-        sig = typing.get_type_hints(factory.__init__)
+    def build_impl(self, registration):
         args = {
             k: self.resolve(v)
-            for k, v in sig.items()
-            if k != 'return' and k not in resolve_args
+            for k, v in registration.needs.items()
+            if k != 'return' and k not in registration.args
 
         }
-        args.update(resolve_args)
+        args.update(registration.args)
 
-        return factory(**args)
+        return registration.builder(**args)
 
     def resolve(self, service_key):
         impls = self.registrations[service_key]
         if len(impls) == 0:
             raise MissingDependencyException()
 
-        factory, args = impls[-1]
-        return self.build_impl(factory, args)
+        registration = impls[-1]
+        return self.build_impl(registration)
