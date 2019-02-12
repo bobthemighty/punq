fc5c4cd63c3d619529714c1a919dbf0403d839d0
Nikita Sobolev <mail@sobolevn.me>
2019-02-12 15:51:04 +0300

    test: adds doctest execution to the existing examples (#14)

--- punq/_compat.py
+++ punq/_compat.py
@@ -1,9 +1,18 @@
-from typing import ForwardRef
+import sys
+
+if sys.version_info >= (3, 7, 0):
+    from typing import ForwardRef
+
+    GenericListClass = list
+else:
+    from typing import List, _ForwardRef as ForwardRef
+
+    GenericListClass = List
 
 
 def is_generic_list(service):
     try:
-        return service.__origin__ == list
+        return service.__origin__ == GenericListClass
     except AttributeError:
         return False
 
