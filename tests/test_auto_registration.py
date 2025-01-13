94fcecc96b114462af88bcf0d220df00a1bacf5d
Bob Gregory <bob@carbonre.com>
2025-01-13 09:19:44 +0000

    Opt-in to auto-registration

--- tests/test_auto_registration.py
+++ tests/test_auto_registration.py
@@ -13,5 +13,5 @@ class Initializable:
         self.dep = dep
 
 def test_when_requesting_an_initialisable_object_that_is_not_in_the_container():
-    container = Container()
+    container = Container(auto_register=True)
     expect(container.resolve(Initializable)).to(be_a(Initializable))
